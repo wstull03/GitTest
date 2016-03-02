b7466820c26e02985d67b9343bd24c9b94e184b5
Your Name <you@example.com>
2016-03-02 10:45:48 -0500

    set up repo

---
+++ ConsoleApplicationGitTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cats = 7;
+            int dogs = 4;
+            int animals = cats * dogs;
+            Console.WriteLine(animals);
+
+            Console.ReadKey();
+
+        }
+    }
+}
